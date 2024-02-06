613e2decbc342b5698bcbcf74fece8c81a1801c6
Oleksandr Siukov <as-service2008@ukr.net>
2024-02-06 11:29:06 +0200

    put and edit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const morgan = require('morgan')
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -30,6 +31,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -61,7 +64,40 @@ app.get('/posts/:id', (req, res) => {
         });
 });
 
+app.delete('/posts/:id', (req, res) => {
+    Post
+        .findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
 
+app.get('/edit/:id', (req, res) => {
+    const title = 'Edit Post';
+    Post
+        .findById(req.params.id)
+        .then(post => res.render(createPath('edit-post'), { post, title }))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
+
+app.put('/edit/:id', (req, res) => {
+    const { title, author, text } = req.body;
+    const { id } = req.params;
+    Post
+        .findByIdAndUpdate(id, { title, author, text })
+        .then((result) => res.redirect(`/posts/${id}`))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error' });
+        });
+});
 
 app.get('/posts', (req, res) => {
     const title = 'Posts';
